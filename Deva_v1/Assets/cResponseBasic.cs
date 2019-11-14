91ae861916f059d755ac7bc0ca5a9db5c9c3e68f
CreepyS <43405227+CreepyS@users.noreply.github.com>
2019-11-14 17:20:21 +0100

    Preyecto Deva_V1

---
+++ Deva_v1/Assets/cResponseBasic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cResponseBasic
+{
+    public List<string> cResponse = new List<string>();
+
+    public cResponseBasic(List<string> cResponse)
+    {
+        this.cResponse = cResponse;
+    }
+
+}
